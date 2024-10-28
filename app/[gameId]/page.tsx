3eb833c193e395710794f22e009dce5018f8f5d5
NooobtimeX <nooobtimex@gmail.com>
2024-10-28 15:53:37 +0700

    error deployment

--- app/[gameId]/page.tsx
+++ app/[gameId]/page.tsx
@@ -1,15 +1,13 @@
 "use client";
-import { use } from "react";
 import ComboMaker from "@/components/ComboMaker";
 
-export default function GamePage(props: {
-  params: Promise<{ gameId: string }>;
-}) {
-  const params = use(props.params);
+export default function GamePage(props: { params: { gameId: string } }) {
+  const { gameId } = props.params;
+
   return (
     <div>
-      <link rel="canonical" href={`https://combomaker.net/${params.gameId}/`} />
-      <ComboMaker gameId={params.gameId} />
+      <link rel="canonical" href={`https://combomaker.net/${gameId}/`} />
+      <ComboMaker gameId={gameId} />
     </div>
   );
 }
